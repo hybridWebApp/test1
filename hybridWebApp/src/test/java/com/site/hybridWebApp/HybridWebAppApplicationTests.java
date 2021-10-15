3a0cd9d467ef532e06d323707985f0dc44fa2edb
KyuSang Cho <81474598+DevKS-Yet@users.noreply.github.com>
2021-10-15 20:53:19 +0900

    you merge (#2)
    * Initial commit
    
    * Delete README.md
    
    * test3
    
    * test3
    
    * test3
    
    * test3
    
    * test4
    
    Co-authored-by: 한국직업전문학교 <한국직업전문학교@DESKTOP-LHHIDOO>
    Co-authored-by:  <asdasd@gmail.com>

---
+++ hybridWebApp/src/test/java/com/site/hybridWebApp/HybridWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.site.hybridWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HybridWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
