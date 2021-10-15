0abe71303ce2e4fb3572580772137a4b39606237
한국직업전문학교 <@DESKTOP-LHHIDOO>
2021-10-15 20:22:07 +0900

    test3

---
+++ hybridWebApp/src/main/java/com/site/hybridWebApp/HybridWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.site.hybridWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HybridWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HybridWebAppApplication.class, args);
+	}
+
+}
